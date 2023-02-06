4884f9c42b8fe30e8950dde5f538282ca95fb6d9
Jonathan <jonathandagan12@gmail.com>
2023-02-06 14:21:37 +0200

    updated the service

--- src/services/ItunesApi.ts
+++ src/services/ItunesApi.ts
@@ -1,9 +1,22 @@
 import axios from 'axios';
 
-const API_URL = 'https://itunes.apple.com/search';
-
-export class ApiService {
-  static search(term: string) {
-    return axios.get(`${API_URL}?term=${term}`);
-  }
+export interface Track {
+  trackId: number;
+  trackName: string;
+  collectionName: string;
+  artistName: string;
+  artworkUrl60: string;
 }
+
+interface Response {
+  results: Track[];
+}
+
+const API_ENDPOINT = 'https://itunes.apple.com/search';
+
+export const fetchTracks = async (term: string) => {
+  const res = await axios.get<Response>(API_ENDPOINT, {
+    params: { term, entity: 'song' },
+  });
+  return res.data.results;
+};
