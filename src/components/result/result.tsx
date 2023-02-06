3371590c675a8248ecc316be0b7cf0daa0d19921
Jonathan <jonathandagan12@gmail.com>
2023-02-06 22:22:53 +0200

    fixed href error

--- src/components/result/result.tsx
+++ src/components/result/result.tsx
@@ -1,8 +1,8 @@
-import React, { useRef, useState } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
+import { Link, BrowserRouter as Router } from "react-router-dom";
 import { Track } from "../../services/ItunesApi";
 
-import { AudioPreview } from '../audio-preview/audio-preview'
+import { AudioPreview } from "../audio-preview/audio-preview";
 
 import styles from "./result.module.scss";
 
@@ -11,16 +11,18 @@ export interface ResultProps {
 }
 
 export const Result: React.FC<ResultProps> = ({ result }) => (
-    <div className={styles["resultCard"]}>
-      <img src={result.artworkUrl60} alt={result.trackName} />
-      <div className={styles["trackAndArtist"]}>
+  <div className={styles["resultCard"]}>
+    <img src={result.artworkUrl60} alt={result.trackName} />
+    <div className={styles["trackAndArtist"]}>
+      <Router>
         <Link to={`/track/${result.trackId}`}>
-            <h2>{result.trackName}</h2>
+          <h2>{result.trackName}</h2>
         </Link>
-        <p>{result.artistName}</p>
-      </div>
-        <AudioPreview src={result.previewUrl} />
+      </Router>
+      <p>{result.artistName}</p>
     </div>
+    <AudioPreview src={result.previewUrl} />
+  </div>
 );
 
 export default Result;
