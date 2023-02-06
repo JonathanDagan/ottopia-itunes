ef8f5aaa3cfdb101a9d33af5b1ef5583089348d0
Jonathan <jonathandagan12@gmail.com>
2023-02-06 22:12:45 +0200

    ffs cba the same message as last commit

--- src/utils.ts
+++ src/utils.ts
@@ -4,3 +4,11 @@ export function formatMsToMMSS(ms: number): string {
   const seconds = totalSeconds % 60;
   return `${minutes}:${seconds.toString().padStart(2, "0")}`;
 }
+
+export const debounce = (fn: Function, ms = 500) => {
+  let timeoutId: ReturnType<typeof setTimeout>;
+  return function (this: any, ...args: any[]) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => fn.apply(this, args), ms);
+  };
+};
