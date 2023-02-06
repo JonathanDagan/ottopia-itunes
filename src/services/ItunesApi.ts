d4f4cf48595f44950339c74fb662fbe35475e55b
Jonathan <jonathandagan12@gmail.com>
2023-02-06 19:57:08 +0200

    remove axios and added single fetch by id

--- src/services/ItunesApi.ts
+++ src/services/ItunesApi.ts
@@ -1,5 +1,3 @@
-import axios from 'axios';
-
 export interface Track {
   trackId: number;
   trackName: string;
@@ -9,15 +7,17 @@ export interface Track {
   previewUrl: string;
 }
 
-interface Response {
-  results: Track[];
-}
-
 const API_ENDPOINT = 'https://itunes.apple.com/search';
 
-export const fetchTracks = async (term: string) => {
-  const res = await axios.get<Response>(API_ENDPOINT, {
-    params: { term, entity: 'song' },
-  });
-  return res.data.results;
+export const fetchTrack = async (id: number): Promise<Track> => {
+  const res = await fetch(`${API_ENDPOINT}?id=${id}&entity=song`);
+  const { results } = await res.json();
+  return results[0];
 };
+
+
+export const fetchTracks = async (term: string): Promise<Track[]> => {
+  const res = await fetch(`${API_ENDPOINT}?term=${term}&entity=song`);
+  const { results } = await res.json();
+  return results;
+}
