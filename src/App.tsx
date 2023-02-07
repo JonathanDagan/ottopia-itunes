3713fcf67bd759e538cac9a322ec0d8076d4979f
Jonathan <jonathandagan12@gmail.com>
2023-02-07 12:24:24 +0200

    formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
 import "./App.css";
 
-import { LandingPage } from './routes/landing-page/landing-page';
-import { SearchView } from './routes/search-view/search-view';
-import { TrackPage } from './routes/track-page/track-page';
-
+import { LandingPage } from "./routes/landing-page/landing-page";
+import { SearchView } from "./routes/search-view/search-view";
+import { TrackPage } from "./routes/track-page/track-page";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<LandingPage/>} />
-        <Route path="/search" element={<SearchView/>}></Route>
-        <Route path="/track/:id" element={<TrackPage/>}></Route>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/search" element={<SearchView />}></Route>
+        <Route path="/track/:id" element={<TrackPage />}></Route>
       </Routes>
     </div>
   );
