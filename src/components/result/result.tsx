52a4bbb64dcfba4ce304f8987fca82be7ca96972
Jonathan <jonathandagan12@gmail.com>
2023-02-07 10:51:42 +0200

    wip

--- src/components/result/result.tsx
+++ src/components/result/result.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Track } from "../../services/ItunesApi";
 
 import { AudioPreview } from "../audio-preview/audio-preview";
+import { formatMsToMMSS } from "../../utils";
 
 import styles from "./result.module.scss";
 
@@ -15,6 +16,7 @@ export const Result: React.FC<ResultProps> = ({ result }) => (
     <div className={styles["trackAndArtist"]}>
             <a href={`/track/${result.trackId}`}>
                 <h2>{result.trackName}</h2>
+                <div className={styles["songLength"]}>{formatMsToMMSS(result.trackTimeMillis)}</div>
             </a>
       <p>{result.artistName}</p>
     </div>
